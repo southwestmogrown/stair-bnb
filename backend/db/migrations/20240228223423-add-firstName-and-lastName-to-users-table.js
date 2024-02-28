fb5abbe9965b04047e6f2087dec96d3414b94410
southwestmogrown <southwestmogrown@gmail.com>
2024-02-28 17:29:46 -0600

    fix schema

--- backend/db/migrations/20240228223423-add-firstName-and-lastName-to-users-table.js
+++ backend/db/migrations/20240228223423-add-firstName-and-lastName-to-users-table.js
@@ -1,6 +1,12 @@
 "use strict";
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn("User", "firstName", {
@@ -15,6 +21,7 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
     await queryInterface.removeColumn("User", "firstName");
     await queryInterface.removeColumn("User", "lastName");
   },
