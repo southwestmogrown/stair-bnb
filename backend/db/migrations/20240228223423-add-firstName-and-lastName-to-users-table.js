ddbb0d64f03b8468f3cfa5c01ee864d4f28955ef
southwestmogrown <southwestmogrown@gmail.com>
2024-02-28 17:24:07 -0600

    fix firstname lastname migration

--- backend/db/migrations/20240228223423-add-firstName-and-lastName-to-users-table.js
+++ backend/db/migrations/20240228223423-add-firstName-and-lastName-to-users-table.js
@@ -3,19 +3,19 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Users", "firstName", {
+    await queryInterface.addColumn("User", "firstName", {
       type: Sequelize.STRING(30),
       defaultValue: "",
     });
 
-    await queryInterface.addColumn("Users", "lastName", {
+    await queryInterface.addColumn("User", "lastName", {
       type: Sequelize.STRING(100),
       defaultValue: "",
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Users", "firstName");
-    await queryInterface.removeColumn("Users", "lastName");
+    await queryInterface.removeColumn("User", "firstName");
+    await queryInterface.removeColumn("User", "lastName");
   },
 };
