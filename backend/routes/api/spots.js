52bb24dce6cfe20185addae64da401139785799c
southwestmogrown <southwestmogrown@gmail.com>
2024-02-29 10:56:02 -0600

    added create Spot post route and validators

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const { Spot, SpotImage, User } = require("../../db/models");
 
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -48,6 +51,63 @@ router.get("/:spotId", async (req, res, next) => {
   };
 
   res.json(safeObj);
+});
+
+const validateSpot = [
+  check("address")
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required"),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
+  check("country")
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required"),
+  check("lat")
+    .exists({ checkFalsy: true })
+    .isDecimal({
+      max: 90,
+      min: -90,
+    })
+    .withMessage("Latitude must be within -90 and 90"),
+  check("state")
+    .exists({ checkFalsy: true })
+    .isDecimal({
+      max: 180,
+      min: -180,
+    })
+    .withMessage("Longitude must be within -180 and 180"),
+  check("name")
+    .exists({ checkFalsy: true })
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Name must be less than 50 characters"),
+  check("description")
+    .exists({ checkFalsy: true })
+    .withMessage("Description is required"),
+  check("price")
+    .exists({ checkFalsy: true })
+    .isDecimal({ min: 0 })
+    .withMessage("Price per day must be a positive number"),
+  handleValidationErrors,
+];
+
+router.post("/", validateSpot, async (req, res) => {
+  const { address, city, state, country, lat, lng, name, description, price } =
+    req.body;
+
+  const newSpot = await Spot.create({
+    ownerId: req.user.id,
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    name,
+    description,
+    price,
+  });
+
+  res.json(newSpot);
 });
 
 module.exports = router;
