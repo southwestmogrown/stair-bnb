c79c313546b343a1e5583d14352044d672f1b128
southwestmogrown <southwestmogrown@gmail.com>
2024-03-01 08:47:17 -0600

    Finished Review feature routes

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const { requireAuth } = require("../../utils/auth");
 
@@ -57,6 +59,105 @@ router.get("/current", requireAuth, async (req, res) => {
   });
 
   res.json(reviewList);
+});
+
+router.post("/:reviewId/images", requireAuth, async (req, res, next) => {
+  const review = await Review.findByPk(req.params.reviewId, {
+    include: {
+      model: Spot,
+      include: SpotImage,
+    },
+  });
+
+  if (!review) {
+    const err = new Error("Review couldn't be found");
+    err.status = 404;
+    err.title = "Review doesn't exist";
+    err.stack = null;
+    next(err);
+  }
+
+  const safeReview = review.toJSON();
+
+  if (safeReview.Spot.SpotImages >= 10) {
+    const err = new Error(
+      "Maximum number of images for this resource was reached"
+    );
+    err.status = 403;
+    err.title = "SpotImage Overflow";
+    err.stack = null;
+    next(err);
+  }
+
+  const { url, preview } = req.body;
+
+  const newSpotImage = await SpotImage.create({
+    spotId: safeReview.spotId,
+    url,
+    preview,
+  });
+
+  res.json({
+    id: newSpotImage.id,
+    url: newSpotImage.url,
+  });
+});
+
+const validateUpdateReview = [
+  check("review")
+    .exists({ checkFalsy: true })
+    .withMessage("Review text is required"),
+  check("stars")
+    .exists({ checkFalsy: true })
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Stars must be an integer from 1 to 5"),
+  handleValidationErrors,
+];
+
+router.put(
+  "/:reviewId",
+  validateUpdateReview,
+  requireAuth,
+  async (req, res, next) => {
+    const reviewToUpdate = await Review.findByPk(req.params.reviewId);
+    const { review, stars } = req.body;
+
+    if (!reviewToUpdate) {
+      const err = new Error("Review couldn't be found");
+      err.status = 404;
+      err.title = "Review doesn't exist";
+      err.stack = null;
+      next(err);
+    }
+
+    if (review !== undefined) reviewToUpdate.review = review;
+    if (stars !== undefined) reviewToUpdate.stars = stars;
+
+    await reviewToUpdate.save();
+
+    const obj = reviewToUpdate.toJSON();
+
+    obj.createdAt = new Date(obj.createdAt).toUTCString();
+    obj.updatedAt = new Date(obj.updatedAt).toUTCString();
+
+    res.json(obj);
+  }
+);
+
+router.delete("/:reviewId", requireAuth, async (req, res, next) => {
+  const reviewToDelete = await Review.findByPk(req.params.reviewId);
+
+  if (!reviewToDelete) {
+    const err = new Error("Review couldn't be found");
+    err.status = 404;
+    err.title = "Review doesn't exist";
+    err.stack = null;
+    next(err);
+  }
+
+  await reviewToDelete.destroy();
+
+  res.json({ message: "Successfully deleted" });
 });
 
 module.exports = router;
