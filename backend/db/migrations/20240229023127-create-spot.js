8ce982d1414588f5b45037f22235b53d00b4f72d
southwestmogrown <southwestmogrown@gmail.com>
2024-03-01 20:34:48 -0600

    fix varchar length for state in Spots migration

--- backend/db/migrations/20240229023127-create-spot.js
+++ backend/db/migrations/20240229023127-create-spot.js
@@ -33,7 +33,7 @@ module.exports = {
         },
         state: {
           allowNull: false,
-          type: Sequelize.STRING(2),
+          type: Sequelize.STRING(50),
         },
         country: {
           allowNull: false,
